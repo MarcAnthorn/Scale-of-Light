3a8b71d4402365c274bc2e6eaf84d699791d19ed
MarcAnthorn <137853745+MarcAnthorn@users.noreply.github.com>
2024-10-16 11:13:53 +0800

    Second_Modified_10_16_OnMac
     最新一次的修改，更新了管道的表现，使用传入点+传出点的形式的传送

--- Assets/Scripts/BaseScripts/BasePipe.cs
+++ Assets/Scripts/BaseScripts/BasePipe.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
+//如果子对象的 Collider 触发了事件，并且父对象有 Rigidbody，事件会自动传递到 Rigidbody 所在的对象；
+//这样就能通过子对象的BoxCollider触发父对象的Trigger响应检测函数了
 public abstract class BasePipe : MonoBehaviour
 {
     protected BoxCollider2D entryTrigger;
