e3822556b4938dba84dc7d52fff1984c0c5d6df4
MarcAnthorn <137853745+MarcAnthorn@users.noreply.github.com>
2024-10-26 19:18:45 +0800

    Debuged Version
    修复了Mask事件过场景被清除但是不被加回来导致的事件空调用；

--- Assets/Scripts/UIScripts/ControllerScripts/MaskTransform.cs
+++ Assets/Scripts/UIScripts/ControllerScripts/MaskTransform.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MaskTransform : MonoBehaviour
 {
     private Vector2 transformVector = new Vector2(1, 1);
     private RectTransform rect;
     private UnityAction action;
+
     private void OnEnable()
     {
-        EventHub.Instance.AddEventListener<UnityAction>("MaskTransformation", MaskTransformation);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    
     }
 
     private void OnDisable()
     {
-        EventHub.Instance.RemoveEventListener<UnityAction>("MaskTransformation", MaskTransformation);
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
+
+    //加载场景的时候调用的方法：
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 检查加载的场景名称
+        if (scene.name == "StartScene")
+        {
+            // 仅在加载特定场景时执行逻辑
+            //因为每次切换场景EventHub都会清空事件，但是mask是过场景不移除的，因此如果不这样写事件就加不回来，就不能被调用；
+            EventHub.Instance.AddEventListener<UnityAction>("MaskTransformation", MaskTransformation);
+            
+        }
+    }
+
 
     /// <summary>
     /// 执行遮罩变化的方法
@@ -25,6 +42,7 @@ public class MaskTransform : MonoBehaviour
     /// <param name="_action">执行完遮罩变化之后的函数，用于处理遮罩结束之后的逻辑</param>
     public void MaskTransformation(UnityAction _action)
     {
+        Debug.Log("MaskTransform Is Called!");
         rect = this.GetComponent<RectTransform>();
         action = _action;
         LeanTween.value(gameObject, rect.sizeDelta.x, 2700, 0.35f).setOnUpdate((float value) =>
